a715c49edfe2a59e46e2a299fb7e94c81f737263
Jozef Bachorík <jozefbachorik@gmail.com>
2025-08-27 13:41:50 +0200

    -pre pull

--- pages/_app.js
+++ pages/_app.js
@@ -1,28 +1,27 @@
-import "../styles/globals.css";
+import '../styles/globals.css';
 
 //components
-import Layout from "../components/Layout/Layout";
-import Transition from "../components/Transition";
+import Layout from '../components/Layout/Layout';
+import Transition from '../components/Transition';
+
 
 //router
-import { useRouter } from "next/router";
+import { useRouter } from 'next/router';
 
 //framer motion
-import { AnimatePresence, motion } from "framer-motion";
+import { AnimatePresence, motion } from 'framer-motion';
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
 
   return (
     <Layout>
-      <AnimatePresence mode="wait">
-        <motion.div key={router.route} className="h-full md:pb-0 pb-20">
-          <Component {...pageProps} />
+      <AnimatePresence mode='wait'>
+        <motion.div key={router.route} className='h-full md:pb-0 pb-20'>
+          <Transition />
+          <Component {...pageProps}/> 
         </motion.div>
       </AnimatePresence>
-
-      {/* Transition mimo AnimatePresence */}
-      <Transition key={`transition-${router.route}`} />
     </Layout>
   );
 }
